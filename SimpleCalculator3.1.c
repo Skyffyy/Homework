275febf01889d0d87b7a7b0f9282492401e040cf
Skyffyy <skyffiriks@gmail.com>
2025-09-26 10:24:27 +0300

    Create SimpleCalculator3.1.c

---
+++ SimpleCalculator3.1.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int my_atoi(const char *str) {
+    int result = 0, sign = 1;
+    if (*str == '-') { sign = -1; str++; }
+    else if (*str == '+') { str++; }
+    while (*str >= '0' && *str <= '9') {
+        result = result * 10 + (*str - '0');
+        str++;
+    }
+    return sign * result;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 4) {
+        printf("Usage: ./calc <num1> <operator> <num2>\n");
+        return 1;
+    }
+
+    int num1 = my_atoi(argv[1]);
+    int num2 = my_atoi(argv[3]);
+    char op = argv[2][0];
+    int result;
+
+    switch (op) {
+        case '+': result = num1 + num2; break;
+        case '-': result = num1 - num2; break;
+        case '*': result = num1 * num2; break;
+        case '/':
+            if (num2 == 0) {
+                printf("Error: Division by zero\n");
+                return 1;
+            }
+            result = num1 / num2; break;
+        default:
+            printf("Error: Unknown operator '%c'\n", op);
+            return 1;
+    }
+
+    printf("Result: %d\n", result);
+    return 0;
+}
