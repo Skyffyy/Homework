6427941df43d7b6364c881a43245579a58e9128f
Skyffyy <skyffiriks@gmail.com>
2025-10-01 14:56:26 +0300

    FileSearchTool6.3.c

--- FileSearchTool6.3.c
+++ FileSearchTool6.3.c
@@ -1,26 +1,5 @@
 #include <stdio.h>
-#include "mystring.h"
-
-int contains_word(const char *line, const char *word, int case_insensitive) {
-    int len_line = my_strlen(line);
-    int len_word = my_strlen(word);
-    if (len_word == 0) return 1;
-
-    for (int i = 0; i <= len_line - len_word; i++) {
-        int match = 1;
-        for (int j = 0; j < len_word; j++) {
-            char c1 = line[i + j];
-            char c2 = word[j];
-            if (case_insensitive) {
-                if (c1 >= 'A' && c1 <= 'Z') c1 += 'a' - 'A';
-                if (c2 >= 'A' && c2 <= 'Z') c2 += 'a' - 'A';
-            }
-            if (c1 != c2) { match = 0; break; }
-        }
-        if (match) return 1;
-    }
-    return 0;
-}
+#include "mystring.h" 
 
 int main(int argc, char *argv[]) {
     int case_insensitive = 0;
